5232ee6f161bd145f38f286bfad7fb31dbdfc86e
Iasmynferreira <iasmynferreira96@gmail.com>
2024-04-03 14:02:12 -0300

    trabalho2

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function ligar() {
+    var lampada = document.getElementById ( 'lampd' );
+    lampada.src ='img/ligada.jpg';
+}
+
+function desligar() {
+    var lampada = document.getElementById ( 'lampd' );
+    lampada.src ='img/desligada.jpg';
+}
